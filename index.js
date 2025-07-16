16280b7d086d1a54a2016ac7a79a9703d74d6afa
abedinalways <nion.sk5067@gmail.com>
2025-07-16 21:06:08 +0600

    deploy in vercel

--- index.js
+++ index.js
@@ -1042,10 +1042,10 @@ async function run() {
     );
 
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 });
-    console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
-    );
+    // await client.db('admin').command({ ping: 1 });
+    // console.log(
+    //   'Pinged your deployment. You successfully connected to MongoDB!'
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
@@ -1055,7 +1055,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('FitForge Running');
+  res.send('FitForge Running on vercel');
 });
 
 app.listen(port, () => {
