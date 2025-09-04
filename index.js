c9a975c1d08de280db2b68b98b9b5eff522a9d8d
abedinalways <nion.sk5067@gmail.com>
2025-09-04 15:37:27 +0600

    solve some deployment issues

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 3000;
 
 // Initialize Firebase Admin SDK
-const serviceAccount = require('./firebase-admin-key.json');
+const admin = require('firebase-admin');
+const serviceAccount = JSON.parse(process.env.FB_SERVICE_KEY);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
